066743fcb2e1334670504bb2780ad1a9907062b3
Kyle Steckbauer <kesteckb@gmail.com>
2022-10-26 22:45:57 -0700

    was able to get the coordinates for objects in the image. Left colors in for checking other images

--- image_scan.py
+++ image_scan.py
@@ -27,39 +27,77 @@ def make_box(box: list) -> list:
 
     return box
 
+# Average corners
+def corner_average(pair, corners, useful_corners):
+    x_coords = []
+    y_coords = []
+    x, y = pair
+    for corner in corners:
+        corner_x, corner_y = corner
+        if abs(corner_x - x) < 4 and abs(corner_y - y) < 4:
+            x_coords.append(corner_x)
+            y_coords.append(corner_y)
+        print(x_coords, y_coords)
+    useful_corners.append((round(mean(x_coords)), round(mean(y_coords))))
+    return useful_corners
+
 # Create a list of coordinates for pixels in the bottom corners of the detected boxes
 def find_corners(box: list, corners:list) -> list:
     for coordinate in box:
         x, y = coordinate
         # TODO: Check for inbounds
-        offsets = [5, 7]
+        offsets = [5, 6, 7, 8]
         for offset in offsets:
             # Checks that the pixel is in bottom corners based on surrounding yellow pixels
             if (x, y - offset) in box and (((x - offset, y) in box) or ((x + offset, y) in box)):
-                pix[x, y] = (255, 0, 0)
+                pix[x, y] = (255, 0, 0)             
                 corners.append(coordinate)
     
     return corners
 
 # Group corner pixels together
-def get_ref_points(corners, ref_points):
+def get_ref_points(corners: list, ref_points: list) -> list:
     for idx, coordinate in enumerate(corners):
         x, y = coordinate
         for num in range(idx + 1, len(corners)):
             x_2, y_2 = corners[num]               
-            if (x_2 < (x - 10) or x_2 > (x + 10)) and (y_2 >= (y - 1) and y_2 <= (y + 1)):
+            if (x_2 < (x - 10) or x_2 > (x + 10)) and (y_2 >= (y - 2) and y_2 <= (y + 2)):
                 if (coordinate, (x_2, y_2)) not in ref_points:
                     ref_points.append((coordinate, (x_2, y_2)))
                     pix[x, y] = (0, 0, 255)
     
     return ref_points
 
+def neighbor_present(bogeys: list, point: tuple) -> bool:
+    x, y = point
+    
+    if not bogeys:
+        return False
 
+    for bogey in bogeys:
+        coord_x, coord_y = bogey
+        if abs(coord_x - x) < 5 and abs(coord_y - y) < 5:
+            return True
+    
+    return False
 
 # Main
 box = make_box([])
 corners = find_corners(box, [])
 ref_points = get_ref_points(corners, [])
+
+bogeys = []
+for pair in ref_points:
+    first_point, second_point = pair
+    first_x, first_y = first_point
+    second_x, second_y = second_point
+
+    midpoint = (round((first_x + second_x) / 2), round((first_y + second_y) / 2))
+
+    if not neighbor_present(bogeys, midpoint):
+        pix[midpoint[0], midpoint[1]] = (255, 0, 255)
+        bogeys.append(midpoint)
+
+
+print(bogeys)
 im.save("new.png")
-    
-    
